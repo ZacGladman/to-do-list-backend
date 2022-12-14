7603ff93ba93c8224b94eccfd16f8a547ac15105
Zac Gladman <zacharygladman@yahoo.co.uk>
2022-12-14 16:19:28 +0000

    added (broken) post request

--- src/db.ts
+++ src/db.ts
@@ -1,4 +1,10 @@
 import { Client } from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+if (!process.env.DATABASE_URL) {
+  throw "No DATABASE_URL env var!  Have you made a .env file?  And set up dotenv?";
+}
 
 const client = new Client({  
     connectionString: process.env.DATABASE_URL,
@@ -6,3 +12,5 @@ const client = new Client({
       rejectUnauthorized: false,
     },
   });
+
+export default client
--- src/server.ts
+++ src/server.ts
@@ -1,30 +1,52 @@
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
-import { Client } from "pg";
+import client from "./db"
+
 
 const app = express();
 
-
-/** Parses JSON data in a request automatically */
 app.use(express.json());
-/** To allow 'Cross-Origin Resource Sharing': https://en.wikipedia.org/wiki/Cross-origin_resource_sharing */
+
 app.use(cors());
 
-// read in contents of any environment variables in the .env file
-dotenv.config();
+
+
+
+
+//ROUTES//
+
+// create a to-do
+
+app.post("/todos", async (req, res) => {
+
+    const todoInfo = req.body;
+    console.log(todoInfo)
+
+    const sqlQuery = 'INSERT INTO to_do (description, due_date, importance, status) VALUES ($1, $2, $3, $4) RETURNING *' 
+    console.log(todoInfo.description, todoInfo.due_date, todoInfo.importance, todoInfo.status)
+    
+    try {
+      console.log('trying query')
+      const newTodo = await client.query(sqlQuery, [todoInfo.description, todoInfo.due_date, todoInfo.importance, todoInfo.status]);
+      res.status(200).json(newTodo)
+      console.log('query completed')
+    } catch (error: any) {
+      console.log('caught error')
+      console.error(error)
+    }
+    
+
+
+
+})
+
+// get all to-dos
+// get one to-do
+// update a to-do
+// delete a to-do
 
 // use the environment variable PORT, or 4000 as a fallback
 const PORT_NUMBER = process.env.PORT ?? 8000;
-
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-});
-
-
 
 app.listen(PORT_NUMBER, () => {
   console.log(`Server is listening on port ${PORT_NUMBER}!`);
