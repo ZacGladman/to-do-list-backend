ffeb103465f02cfd4db7b56b1d7cd64420464f5a
Zac Gladman <zacharygladman@yahoo.co.uk>
2023-01-02 14:49:46 +0000

    test

--- src/db.ts
+++ src/db.ts
@@ -1,20 +1,19 @@
 import { Client } from "pg";
 import dotenv from "dotenv";
 
-
 dotenv.config();
 
 if (!process.env.DATABASE_URL) {
   throw "No DATABASE_URL env var!  Have you made a .env file?  And set up dotenv?";
 }
 
-const client = new Client({  
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  });
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
-client.connect()
+client.connect();
 
-export default client
+export default client;
